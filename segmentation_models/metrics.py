d96a9984f74695f8827abc0939a229624674bb7d
GianlucaCarlini <gianluca.carlini@outlook.it>
2022-11-07 15:46:53 +0100

    Added a main file for testing

--- segmentation_models/metrics.py
+++ segmentation_models/metrics.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 
 
-def iou_score(y_true, y_pred, threshold=None):
+EPSILON = tf.keras.backend.epsilon()
+
+
+def mean_iou(y_true, y_pred, threshold=None):
 
     if threshold is not None:
         y_pred = tf.greater(y_pred, threshold)
-        y_pred = tf.cast(y_pred, tf.floatx())
+        y_pred = tf.cast(y_pred, tf.float32())
 
     intersection = tf.reduce_sum(y_true * y_pred, axis=[0, 1, 2])
     union = tf.reduce_sum((y_true + y_pred), axis=[0, 1, 2]) - intersection
 
-    score = (intersection + 1e-5) / (union + 1e-5)
+    score = tf.divide((intersection + EPSILON), (union + EPSILON))
 
     return tf.reduce_mean(score)
 
@@ -19,14 +22,14 @@ def f1_score(y_true, y_pred, threshold=None, beta=1):
 
     if threshold is not None:
         y_pred = tf.greater(y_pred, threshold)
-        y_pred = tf.cast(y_pred, tf.floatx())
+        y_pred = tf.cast(y_pred, tf.float32())
 
     tp = tf.reduce_sum(y_true * y_pred, axis=[0, 1, 2])
     fp = tf.reduce_sum(y_pred, axis=[0, 1, 2]) - tp
     fn = tf.reduce_sum(y_true, axis=[0, 1, 2]) - tp
 
-    score = ((1 + beta**2) * tp + 1e-5) / (
-        (1 + beta**2) * tp + beta**2 * fn + fp + 1e-5
+    score = ((1 + beta**2) * tp + EPSILON) / (
+        (1 + beta**2) * tp + beta**2 * fn + fp + EPSILON
     )
     score = tf.reduce_mean(score)
 
